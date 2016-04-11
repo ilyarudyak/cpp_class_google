12ca0caab8bbf77ec584058fada1afe62d841a6f
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-04-11 22:45:38 +0300

    done ex. 1, 2

---
+++ homework/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void cricket() {
+
+    double numberOfChirps = 0;
+    cout << "Number of chirps: ";
+    cin >> numberOfChirps;
+    cout << "The temperature is: " << setprecision(1) << fixed
+         << (numberOfChirps + 40) / 4 << " degrees" << endl;
+
+}
+
+void exam() {
+
+    double assignment1, assignment2, assignment3, assignment4;
+    double midterm, final, participation;
+
+    cout << "Enter the score for the first assignment: ";
+    cin >> assignment1;
+    cout << "Enter the score for the second assignment: ";
+    cin >> assignment2;
+    cout << "Enter the score for the third assignment: ";
+    cin >> assignment3;
+    cout << "Enter the score for the forth assignment: ";
+    cin >> assignment4;
+    cout << "Enter the score for the midterm: ";
+    cin >> midterm;
+    cout << "Enter the score for the final: ";
+    cin >> final;
+    cout << "Enter the score for the section grade: ";
+    cin >> participation;
+
+    double finalGrade = (assignment1 + assignment2 + assignment3 + assignment4) * .1 +
+                        midterm * .15 + final * .35 + participation * .1;
+    cout << "The final grade is: " << setprecision(1) << fixed << finalGrade << endl;
+}
+
+int main() {
+
+    exam();
+//    cricket();
+    return 0;
+}
