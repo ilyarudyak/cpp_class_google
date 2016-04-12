eb6e5d723c1a37cd7e1816e9a300b6c90a36dcc0
Ilya Rudyak <ilyarudyak@yahoo.com>
2016-04-12 21:40:31 +0300

    done ex. 4

---
+++ next_steps/exercise4/main.cpp
@@ -0,0 +1,67 @@
+// time.cpp, Maggie Johnson
+// Description: A simple time class.
+
+#include <iostream>
+using namespace std;
+
+class Time {
+private:
+    int hours_;
+    int minutes_;
+    int seconds_;
+public:
+    void set(int h, int m, int s) {hours_ = h; minutes_ = m; seconds_ = s; return;}
+    void increment();
+    void decrement();
+    void display();
+};
+
+void Time::decrement() {
+    seconds_--;
+    if (seconds_ == -1) {
+        minutes_--;
+        seconds_ = 59;
+    }
+    if (minutes_ == -1) {
+        hours_--;
+        minutes_= 59;
+    }
+    if (hours_ == -1) {
+        hours_ = 23;
+    }
+    return;
+}
+
+void Time::increment() {
+    seconds_++;
+    minutes_ += seconds_/60;
+    hours_ += minutes_/60;
+    seconds_ %= 60;
+    minutes_ %= 60;
+    hours_ %= 24;
+    return;
+}
+
+void Time::display() {
+    cout << (hours_ % 12 ? hours_ % 12:12) << ':'
+    << (minutes_ < 10 ? "0" :"") << minutes_ << ':'
+    << (seconds_ < 10 ? "0" :"") << seconds_
+    << (hours_ < 12 ? " AM" : " PM") << endl;
+}
+
+int main() {
+    Time timer;
+    timer.set(00,00,02);
+    for (int i = 0; i < 5; i++) {
+        timer.decrement();
+        timer.display();
+        cout << endl;
+    }
+//    timer.set(23,59,58);
+//    for (int i = 0; i < 5; i++) {
+//        timer.increment();
+//        timer.display();
+//        cout << endl;
+//    }
+
+}
